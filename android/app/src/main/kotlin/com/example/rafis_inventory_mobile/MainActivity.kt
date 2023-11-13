0eb0cbbda3ed147646ef88ba21d2496fb5ec3226
fiardiel <rafi.ardiel@ui.ac.id>
2023-11-13 11:46:17 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/rafis_inventory_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rafis_inventory_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/item_card.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:rafis_inventory_mobile/screens/additem_form.dart';
+
+class Item {
+  final String name;
+  final IconData icon;
+  final Color color;
+  Item(this.name, this.icon, this.color);
+}
+
+class ItemCard extends StatelessWidget {
+  final Item item;
+
+  const ItemCard(this.item, {Key? key}); // Constructor
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: item.color,
+      child: InkWell(
+        // Area responsive to touch
+        onTap: () {
+          // Show SnackBar when clicked
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(SnackBar(
+                content: Text("You pressed the ${item.name} button!")));
+
+          // Navigate to the appropriate route (depending on the button type)
+          if (item.name == "Add Item") {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const ItemFormPage(),
+                ));
+          }
+        },
+        child: Container(
+          // Container to hold Icon and Text
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  item.icon,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                const Padding(padding: EdgeInsets.all(3)),
+                Text(
+                  item.name,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
